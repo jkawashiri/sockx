86f3cf4bc43edcb0464c01b4c1c104d36fe14f2b
Darius Grant <d.grant065@gmail.com>
2023-09-15 17:29:15 -0400

    create shoe model

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Shoe(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=25)
+    size = models.IntegerField()
+    colorway = models.CharField(max_length=15)
+    description = models.TextField(max_length=200)
+    release_date = models.DateField('Release Date')
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+
+
+
+
