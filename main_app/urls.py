4ce5692b16619cefa21ca3bec01d5e6267f6f91e
jkawashiri <jkawashiri5@gmail.com>
2023-09-15 15:52:43 -0700

    implements adding shoe feature and displays shoes on page

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('shoes/', views.ShoeList.as_view(), name='index'),
+    path('shoes/<int:pk>/', views.ShoeDetail.as_view(), name='detail'),
+    path('shoes/create/', views.ShoeCreate.as_view(), name='shoes_create'),
 ]
