54a6611db854a31ed94119283711e5d612aed1bc
Darius Grant <d.grant065@gmail.com>
2023-09-21 18:39:20 -0400

    update searchbar

--- main_app/views.py
+++ main_app/views.py
@@ -24,7 +24,8 @@ def search_shoes(request):
    if request.method == "POST":
       searched = request.POST['searched']
       shoes = Shoe.objects.filter(name__icontains=searched)
-      return render(request, 'shoes/search_shoes.html', {'searched':searched, 'shoes':shoes})
+      brand = Shoe.objects.filter(brand__icontains=searched)
+      return render(request, 'shoes/search_shoes.html', {'searched':searched, 'shoes':shoes, 'brand':brand})
    else: 
       return render(request, 'shoes/search_shoes.html', {})
 
